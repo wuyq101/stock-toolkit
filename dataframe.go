28ee781b684dd6c0589a14e3c45999ba73ab4bd6
吴英强 <wuyingqiang@bytedance.com>
2018-01-25 16:18:10 +0800

    add ma ema
    Change-Id: Ie3a47e5b1ede652abe45de953f205bdc848daa75

---
+++ dataframe.go
@@ -0,0 +1,5 @@
+package stk
+
+// DataFrame support python pandas library dataframe
+type DataFrame struct {
+}
---
+++ toolkit.go
@@ -0,0 +1,16 @@
+package stk
+
+// MA 计算MA的工具函数
+// 移动平均线，Moving Average，简称MA，MA是用统计分析的方法，将一定时期内的证券价格（指数）加以平均，
+// 并把不同时间的平均值连接起来，形成一根MA，用以观察证券价格变动趋势的一种技术指标。
+func MA(data []float64, n int) []float64 {
+	//TODO
+	return nil
+}
+
+// EMA （Exponential Moving Average）是指数平均数指标。
+// 也叫EXPMA指标，它也是一种趋向类指标，指数平均数指标是以指数式递减加权的移动平均。
+func EMA(data []float64, n int) []float64 {
+	//TODO
+	return nil
+}
