edaee668a0c428986adb5c9f2cbfe8eee1b4cf24
吴英强 <wuyingqiang@bytedance.com>
2018-02-01 17:42:04 +0800

    using ema to calc bias
    Change-Id: Ic951ecb1831515c8ba9e7dea71db594e9af2aa59

--- toolkit.go
+++ toolkit.go
@@ -56,7 +56,7 @@ func FloatEqual(a, b float64, tolerance float64) bool {
 // BIAS 乖离率，是用百分比来表示价格与MA间的偏离程度(差距率)。
 // BIAS=(收盘价-收盘价的N日简单平均)/收盘价的N日简单平均*100
 func BIAS(data []float64, n int) []float64 {
-	ma := MA(data, n)
+	ma := EMA(data, n)
 	return Divide(Sub(data, ma), ma)
 }
 
